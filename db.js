cb2a35914826c609e269713b46cda0ca613ddac1
hamzamanar <hamzamanar132@gmail.com>
2024-03-30 11:58:04 +0000

    all comit

---
+++ db.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/ecole', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log('MongoDB connected...'))
+.catch(err => console.log(err));
